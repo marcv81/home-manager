f2cbada7108ff8c88f2aab8b751af139699daaaf
Marc Venturini <marc.venturini@gmail.com>
2026-07-10 02:32:04 +0800

    Added difftastic + tig

--- home.nix
+++ home.nix
@@ -3,15 +3,31 @@
   home = {
     packages = with pkgs; [
       home-manager
+      tig
     ];
 
     username = "user";
     homeDirectory = "/home/user";
 
     stateVersion = "26.05";
+
+    sessionVariables = {
+      LESS = "-SFXR --mouse";
+    };
   };
 
   programs = {
+    git = {
+      enable = true;
+    };
+
+    difftastic = {
+      enable = true;
+      git = {
+        enable = true;
+      };
+    };
+
     bash = {
       enable = true;
       bashrcExtra = ''
