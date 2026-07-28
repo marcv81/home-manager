3ccbe99c952ca2cfd78fd1d20360d9569631a592
Marc Venturini <marc.venturini@gmail.com>
2026-07-29 00:32:23 +0800

    Enabled clipboard integration

--- resources/init.lua
+++ resources/init.lua
@@ -2,6 +2,7 @@ vim.g.mapleader = ' '
 vim.opt.number = true
 vim.opt.signcolumn = 'yes:2'
 vim.opt.termguicolors = true
+vim.opt.clipboard = 'unnamedplus'
 vim.cmd.colorscheme('github_dark_high_contrast')
 
 vim.keymap.set('n', '<leader>e', '<cmd>Neotree toggle<cr>')
