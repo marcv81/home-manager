170a206db47991a590ab600462abb2165a78e392
Marc Venturini <marc.venturini@gmail.com>
2026-07-29 00:27:01 +0800

    Better written init.lua
    - Also implement toggle for <leader>gd/<leader>gD
    - Other minor refactors

--- resources/init.lua
+++ resources/init.lua
@@ -1,38 +1,39 @@
+vim.g.mapleader = ' '
 vim.opt.number = true
-vim.opt.signcolumn = 'yes'
-
-require('github-theme').setup({})
-vim.cmd.colorscheme("github_dark_high_contrast")
-
-vim.g.mapleader = " "
-vim.keymap.set("n", "<Space>", "<Nop>", { silent = true })
+vim.opt.signcolumn = 'yes:2'
+vim.opt.termguicolors = true
+vim.cmd.colorscheme('github_dark_high_contrast')
 
 vim.keymap.set('n', '<leader>e', '<cmd>Neotree toggle<cr>')
 
-require('gitsigns').setup({
-  on_attach = function(bufnr)
-    local gs = package.loaded.gitsigns
-    vim.keymap.set('n', '<leader>hs', gs.stage_hunk, { buffer = bufnr })
-    vim.keymap.set('n', '<leader>hr', gs.reset_hunk, { buffer = bufnr })
-    vim.keymap.set('n', '<leader>gd', gs.diffthis, { buffer = bufnr })
-    vim.keymap.set('n', '<leader>gD', function() gs.diffthis('HEAD') end, { buffer = bufnr })
-  end
-})
+local gs = require('gitsigns')
+vim.keymap.set('n', '<leader>hs', gs.stage_hunk)
+vim.keymap.set('n', '<leader>hr', gs.reset_hunk)
 
--- Blame toggle
-vim.keymap.set('n', '<leader>gb', function()
-  local bufname = vim.api.nvim_buf_get_name(0)
-  if vim.startswith(bufname, 'gitsigns-blame:') then
-    vim.cmd('q')
-    return
-  end
-  local filename = vim.fn.fnamemodify(bufname, ':t')
-  for _, win in ipairs(vim.api.nvim_list_wins()) do
+local function toggle(prefix, action)
+  local win_closed = false
+  -- If there are multiple windows matching the prefix, close them all.
+  for _, win in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
     local win_name = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(win))
-    if vim.startswith(win_name, 'gitsigns-blame:') and win_name:find(filename, 1, true) then
+    if vim.startswith(win_name, prefix) then
       vim.api.nvim_win_close(win, true)
-      return
+      win_closed = true
     end
   end
-  require('gitsigns').blame()
+  -- Only call the action if we didn't find any window to close.
+  if not win_closed then
+    action()
+  end
+end
+
+vim.keymap.set('n', '<leader>gd', function()
+  toggle('gitsigns:', gs.diffthis)
+end)
+
+vim.keymap.set('n', '<leader>gD', function()
+  toggle('gitsigns:', function() gs.diffthis('HEAD') end)
+end)
+
+vim.keymap.set('n', '<leader>gb', function()
+  toggle('gitsigns-blame:', gs.blame)
 end)
