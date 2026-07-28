7daa6feaf547dcee24ef6bd551d20e969d630297
Marc Venturini <marc.venturini@gmail.com>
2026-07-29 01:01:29 +0800

    Mouse scroll brings the cursor to the target buffer

--- resources/init.lua
+++ resources/init.lua
@@ -38,3 +38,19 @@ end)
 vim.keymap.set('n', '<leader>gb', function()
   toggle('gitsigns-blame:', gs.blame)
 end)
+
+vim.opt.mouse = 'a'
+
+local function focus_mouse()
+  local win = vim.fn.getmousepos().winid
+  if win and win ~= vim.api.nvim_get_current_win() then
+    vim.api.nvim_set_current_win(win)
+  end
+end
+
+for _, key in ipairs({ '<ScrollWheelUp>', '<ScrollWheelDown>', '<ScrollWheelLeft>', '<ScrollWheelRight>' }) do
+  vim.keymap.set('n', key, function()
+    vim.schedule(focus_mouse)
+    return key
+  end, { expr = true })
+end
