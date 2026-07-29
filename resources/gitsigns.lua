fbdd11d93be38e963c8ea27e8c35d6f591ced2a8
Marc Venturini <marc.venturini@gmail.com>
2026-07-29 23:51:33 +0800

    Ignore errors when failing to close windows
    This helps in the following edge case: the only
    window is a gitsigns window and cannot be closed.

--- resources/gitsigns.lua
+++ resources/gitsigns.lua
@@ -10,7 +10,7 @@ local function close_win(prefix)
   for _, win in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
     local name = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(win))
     if vim.startswith(name, prefix) then
-      vim.api.nvim_win_close(win, true)
+      pcall(vim.api.nvim_win_close, win, true)
       closed = true
     end
   end
